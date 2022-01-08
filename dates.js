94063a3506048ae9e2cdc30b4290efb7ea592558
Hugueprime <hugueprime@gmail.com>
2022-01-08 18:05:30 +0100

    add date math s2

--- dates.js
+++ dates.js
@@ -42,5 +42,40 @@ var dates = {
         {"start": new Date('2021-12-06'), "end": new Date('2021-12-12')},//10
         {"start": new Date('2021-12-06'), "end": new Date('2021-12-12')},
         {"start": new Date('2021-12-13'), "end": new Date('2021-12-19')},
-    ]
+    ],
+    "epita_maths_s2":[
+        {"start": new Date('2024-01-01'), "end": new Date('2024-01-01')},//Introduction aux polynômes
+        {"start": new Date('2024-01-01'), "end": new Date('2024-01-01')},//Division euclidienne
+        {"start": new Date('2024-01-01'), "end": new Date('2024-01-01')},//Racine d’un polynôme et factorisation
+        {"start": new Date('2022-01-10'), "end": new Date('2022-01-16')},//Equations différentielles linéaires du premier ordre
+        {"start": new Date('2022-01-10'), "end": new Date('2022-01-16')},//5 //Equations différentielles linéaires du second ordre
+        {"start": new Date('2024-01-01'), "end": new Date('2024-01-01')},//Continuité en un point
+        {"start": new Date('2024-01-01'), "end": new Date('2024-01-01')},//Continuité sur un intervalle
+        {"start": new Date('2022-01-17'), "end": new Date('2022-01-23')},//Formules de Taylor
+        {"start": new Date('2022-01-17'), "end": new Date('2022-01-23')},//Développements limités au voisinage d'un point
+        {"start": new Date('2022-01-24'), "end": new Date('2022-01-30')},//10 Opérations sur les développements limités
+        {"start": new Date('2022-01-31'), "end": new Date('2022-02-06')},//Introduction aux espaces vectoriels
+        {"start": new Date('2022-01-31'), "end": new Date('2022-02-06')},//Propriétés d’un espace vectoriel
+        {"start": new Date('2022-01-31'), "end": new Date('2022-02-06')},//Définition d’un sous-espace vectoriel
+        {"start": new Date('2022-02-07'), "end": new Date('2022-02-13')},//Combinaisons linéaires, caractérisation et intersection d’espaces vectoriels
+        {"start": new Date('2022-02-07'), "end": new Date('2022-02-13')},//15 Somme, sous-espace vectoriel engendré
+        {"start": new Date('2022-02-07'), "end": new Date('2022-02-13')},//Famille libre
+        {"start": new Date('2022-02-14'), "end": new Date('2022-02-20')},//Famille génératrice
+        {"start": new Date('2022-02-14'), "end": new Date('2022-02-20')},//Base
+        {"start": new Date('2022-02-14'), "end": new Date('2022-02-20')},//Dimension d'un espace vectoriel
+        {"start": new Date('2022-02-14'), "end": new Date('2022-02-20')},//20 Dimension des sous-espaces vectoriels
+        {"start": new Date('2022-03-14'), "end": new Date('2022-03-20')},//Définition d’une application linéaire
+        {"start": new Date('2022-03-14'), "end": new Date('2022-03-20')},//Exemples d’applications linéaires
+        {"start": new Date('2022-03-28'), "end": new Date('2022-04-03')},//Image et noyau d'une application linéaire
+        {"start": new Date('2022-04-04'), "end": new Date('2022-04-10')},//Définition d’une matrice
+        {"start": new Date('2022-04-04'), "end": new Date('2022-04-10')},//25 Multiplication de matrices
+        {"start": new Date('2022-04-11'), "end": new Date('2022-04-17')},//Matrice d'une application linéaire
+        {"start": new Date('2022-05-02'), "end": new Date('2022-05-08')},//Définition de l'inverse d’une matrice
+        {"start": new Date('2022-05-02'), "end": new Date('2022-05-08')},//Calcul de l’inverse d’une matrice
+        {"start": new Date('2024-01-01'), "end": new Date('2024-01-01')},//Matrices triangulaires, matrice transposée, trace, matrices symétriques
+        {"start": new Date('2024-01-01'), "end": new Date('2024-01-01')},//30 Déterminant d'une matrice carrée - Définition
+        {"start": new Date('2024-01-01'), "end": new Date('2024-01-01')},//Introduction aux systèmes linéaires
+        {"start": new Date('2024-01-01'), "end": new Date('2024-01-01')},//Théorie des systèmes linéaires
+        {"start": new Date('2024-01-01'), "end": new Date('2024-01-01')},//Résolution par la méthode du pivot de Gauss
+    ],
 }
