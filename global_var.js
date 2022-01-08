881eee3d706411375516a9803430eafd6a1abb95
Hugueprime <hugueprime@gmail.com>
2022-01-08 18:05:12 +0100

    change default server

--- global_var.js
+++ global_var.js
@@ -1,4 +1,4 @@
 
 const activeVideoPlayerState = "isVideoPlayerActivated";
 const INSTANCE_VIDEO = "instance_video_player";
-const DEFAULT_VIDEO_PLAYER = "invidious.osi.kr";
+const DEFAULT_VIDEO_PLAYER = "invidious.mutahar.rocks";
